2a4d94cadde16b2fd5e77cf6c841143e3b6f5972
Roy Hyunjin Han <rhh@crosscompute.com>
2020-08-14 06:18:30 -0400

    Save temporary

--- server/asset_format_dss/views.py
+++ server/asset_format_dss/views.py
@@ -1,24 +1,25 @@
-from asset_tracker.models.asset import Asset, Connection, Bus, LineType
+from asset_tracker.models.asset import Asset, Connection, LineType
 from io import StringIO
 from pyramid.response import Response
 from pyramid.view import view_config
 
-from .routines.opendss import generate_dss_script, normalize_assets_and_connections, line_types_to_json, \
-    remove_temporal_line_connections
+from .routines.opendss import (
+    generate_dss_script,
+    line_types_to_json,
+    normalize_assets_and_connections,
+    remove_temporal_line_connections)
 
 
 @view_config(
     route_name='assets.dss',
-    request_method='GET')
+    request_method='GET',
+    permission='see')
 def see_assets_dss(request):
     db = request.db
     vsourceId = request.GET.get('sourceId')
-    print(vsourceId)
     element = db.query(Asset).filter(Asset.id == vsourceId)
-    print(element.count())
     connections = db.query(Connection).all()
     assets = db.query(Asset).filter(Asset.is_deleted == False)
-
     line_types = line_types_to_json(db.query(LineType).all())
 
     # Asset filters and transformers
@@ -31,10 +32,8 @@ def see_assets_dss(request):
         script = generate_dss_script(normalized_assets, normalized_conn, buses, line_types)
 
     f = StringIO()
-
     for line in script:
         f.write(line)
-
     return Response(
         body=f.getvalue(),
         status=200,
